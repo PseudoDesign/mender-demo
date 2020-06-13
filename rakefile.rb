194b8039a01a6e40f981da792b8895536528ffcd
Adam <PseudoDesign@users.noreply.github.com>
2020-06-13 10:29:18 -0600

    building dockerfile with rake

--- rakefile.rb
+++ rakefile.rb
@@ -1,12 +1,12 @@
-PROJECT_NAME = mender-demo
+PROJECT_NAME = "mender-demo"
 ROOT_DIRECTORY = File.expand_path(File.dirname(__FILE__))
 
 def mkdir_f(dir)
     mkdir dir unless File.directory?(dir)
 end
 
-
+desc "Build the Dockerfile as #{PROJECT_NAME}-build:latest"
 task :docker do
-    sh "docker build -t #{PROJECT_NAME}-build #{ROOT_DIRECTORY}"
+    sh "docker build -t #{PROJECT_NAME}-build:latest #{ROOT_DIRECTORY}"
 end
 
