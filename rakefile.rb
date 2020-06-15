7031481a21b8dbbc75be58f9e0a573ea89e750f7
Adam <PseudoDesign@users.noreply.github.com>
2020-06-15 00:25:20 -0600

    changing ownership of root directory

--- rakefile.rb
+++ rakefile.rb
@@ -25,7 +25,7 @@ end
 def docker_run_command(command)
     # Execute the provided comlmand in the docker image, with appropriate mounting, secrets, etc
     uid = `id -u`.delete!("\n")
-    run_string = "docker run --rm #{docker_volume_string} -it #{PROJECT_NAME}-build:latest /bin/bash -c \" sudo usermod -u #{uid} #{DOCKER_USER} && chown #{DOCKER_USER}:#{DOCKER_USER} . ; export BITBAKEDIR=sources/poky/bitbake;  #{command} \""
+    run_string = "docker run --rm #{docker_volume_string} -it #{PROJECT_NAME}-build:latest /bin/bash -c \" sudo usermod -u #{uid} #{DOCKER_USER} ; sudo chown #{DOCKER_USER}:#{DOCKER_USER} . ; export BITBAKEDIR=sources/poky/bitbake;  #{command} \""
     sh run_string
 end
 
